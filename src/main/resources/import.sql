90a037399978c36880232759b6e0a359a439f751
Douglas Dias <douglasmarquesdias@hotmail.com>
2018-04-08 07:58:29 -0300

    Inclusão de classe repository para carregar dados do H2

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into operadora (nome, codigo, categoria) values ('Oi', '31', 'Fixo');
+insert into operadora (nome, codigo, categoria) values ('Tim', '14', 'Celular');
+
+insert into contato (nome, telefone, data, operadora_id) values ('Douglas Dias', '99563-9390', sysdate(), 1);
+insert into contato (nome, telefone, data, operadora_id) values ('Debora Oliveira', '99563-9390', sysdate(), 2);
